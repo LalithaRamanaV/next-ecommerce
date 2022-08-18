e4037fc624e1de09dc715a7c86aae0a8b56fca90
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-08-18 16:18:54 +0530

    list products

---
+++ utils/data.js
@@ -0,0 +1,82 @@
+const data = {
+    products:[
+        
+          {
+            name: 'Lipstick',
+            slug: 'Lipstick',
+            category: 'Fashion',
+            image: '/images/lip.jpg',
+            price: 2500,
+            brand: 'Lakme',
+            rating: 3.2,
+            numReviews: 10,
+            countInStock: 20,
+            description:'Red shade Lipstick',
+          },
+          {
+            name: 'Handbag',
+            slug: 'Handbag for women',
+            category: 'Fashion',
+            image: '/images/bag.jpg',
+            price: 4000,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'Handbags for women',
+          },
+          {
+            name: 'Heels',
+            slug: 'Heels for women',
+            category: 'Fashion',
+            image: '/images/heels2.jpg',
+            price: 4000,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'Heels',
+          },
+          {
+            name: 'Heels',
+            slug: 'Heels for women',
+            category: 'Fashion',
+            image: '/images/heels.jpg',
+            price: 4000,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'Heels',
+          },
+          {
+            name: 'Watch',
+            slug: 'watch for women',
+            category: 'Watches',
+            image: '/images/image1.png',
+            price: 3500,
+            brand: 'Daniel Willington',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'Watch with bracelet',
+          },
+        
+         
+          {
+            name: 'Watch',
+            slug: 'watch for men',
+            category: 'Watches',
+            image: '/images/watch3.png',
+            price: 3000,
+            brand: 'Daniel Willington',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'Mens watch',
+          },
+        ],
+      };
+      
+      export default data;
+  
