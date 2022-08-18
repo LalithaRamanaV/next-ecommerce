c59d7be974ee6074ff603627e4c2ac5e2f43446f
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-08-18 19:25:18 +0530

    cart page with add and delete functionality

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,6 +8,7 @@ import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
     const {state, dispatch} = useContext(Store);
+    const router = useRouter();
     const {query} = useRouter();
     const {slug} = query;
     const product = data.products.find(x => x.slug === slug);
@@ -23,7 +24,8 @@ export default function ProductScreen() {
           alert('Sorry. Product is out of stock');
           return;
         }
-     dispatch({type: 'CART_ADD_ITEM', payload: {...product,quantity:1}});
+     dispatch({type: 'CART_ADD_ITEM', payload: {...product,quantity}});
+     router.push('/cart');
     }
   return (
    <Layout title={product.name}>
